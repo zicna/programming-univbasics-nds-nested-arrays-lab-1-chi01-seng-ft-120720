d655688bfd78ece554bc22443e4ca97f663daf7c
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-19 18:02:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,6 +37,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
